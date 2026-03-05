c3fd49016ca1fae10710be6fbb8201f4c8c13ea9
Lawrence Arryl Lopez <160656561+arryllopez@users.noreply.github.com>
2026-03-05 17:10:12 -0500

    utilizing flags to take user input and utilized a map phrasebook and map based testing

--- chapter-2/main.go
+++ chapter-2/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	greeting := greet("en")
+	var lang string
+	flag.StringVar(&lang,
+		"lang",
+		"en",
+		"The required language, e.g. en, ur...")
+	flag.Parse()
+
+	greeting := greet(language(lang))
 	fmt.Println(greeting)
 }
 
@@ -11,15 +21,22 @@ func main() {
 // type safety and it enhances readabiolity
 type language string
 
+// impementing a map <language,phrase>
+var phrasebook = map[language]string{
+	"el": "Χαίρετε Κόσμε",     // Greek
+	"en": "Hello world",       // English
+	"fr": "Bonjour le monde",  // French
+	"he": "שלום עולם",         // Hebrew
+	"ur": "ہیلو دنیا",         // Urdu
+	"vi": "Xin chào Thế Giới", // Vietnamese
+}
+
 // greet says hello to the world in the specified language
 // the inpuyt language will be a string that represents a language
 func greet(l language) string {
-	switch l {
-	case "en":
-		return "Hello world"
-	case "fr":
-		return "Bonjour le monde"
-	default:
-		return ""
+	greeting, ok := phrasebook[l] // returns the message associated with the key language l and a boolean that tells us if the key was found
+	if !ok {                      // if ok is false
+		return fmt.Sprintf("unsupported language: %q", l)
 	}
+	return greeting
 }
